81b41e9c9a42e0114eb7d018c8af4ad92385502f
Yoshidome-Hiro <domehiro5117@gmail.com>
2025-07-16 23:50:37 +0900

    kadai_20_提出

---
+++ kadai_020/event.js
@@ -0,0 +1,5 @@
+const textElement =document.getElementById('text');
+const buttonElement = document.getElementById('btn');
+buttonElement.addEventListener('click', () => {
+ textElement.textContent = 'ボタンをクリックしました';  
+ })
